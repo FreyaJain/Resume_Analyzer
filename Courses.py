5aeeeace97710b7accf032538d68e62f9a1ad538
FreyaJain <150811763+FreyaJain@users.noreply.github.com>
2023-12-30 23:48:23 +0530

    Add files via upload

---
+++ Courses.py
@@ -0,0 +1,38 @@
+ds_course = [['Machine Learning Crash Course by Google [Free]', 'https://developers.google.com/machine-learning/crash-course'],
+             ['Programming for Data Science with Python','https://www.udacity.com/course/programming-for-data-science-nanodegree--nd104'],
+             ['Programming for Data Science with R','https://www.udacity.com/course/programming-for-data-science-nanodegree-with-R--nd118'],
+             ['Introduction to Data Science','https://www.udacity.com/course/introduction-to-data-science--cd0017'],
+             ['Intro to Machine Learning with TensorFlow','https://www.udacity.com/course/intro-to-machine-learning-with-tensorflow-nanodegree--nd230']]
+
+web_course = [['Django Crash course [Free]','https://youtu.be/e1IyzVyrLSU'],
+              ['Python and Django Full Stack Web Developer Bootcamp','https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp'],
+              ['React Crash Course [Free]','https://youtu.be/Dorf8i6lCuk'],
+              ['ReactJS Project Development Training','https://www.dotnettricks.com/training/masters-program/reactjs-certification-training'],
+              ['Front End Web Developer by Udacity','https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011'],
+              ['Become a React Developer by Udacity','https://www.udacity.com/course/react-nanodegree--nd019']]
+
+android_course = [['Android Development for Beginners [Free]','https://youtu.be/fis26HvvDII'],
+                  ['Android App Development Specialization','https://www.coursera.org/specializations/android-app-development'],
+                  ['Building an Android App with Architecture Components','https://www.linkedin.com/learning/building-an-android-app-with-architecture-components'],
+                  ['Android App Development Masterclass using Kotlin','https://www.udemy.com/course/android-oreo-kotlin-app-masterclass/'],
+                  ['Flutter & Dart - The Complete Flutter App Development Course','https://www.udemy.com/course/flutter-dart-the-complete-flutter-app-development-course/'],
+                  ['Flutter App Development Course [Free]','https://youtu.be/rZLR5olMR64']]
+
+ios_course = [['IOS App Development by LinkedIn','https://www.linkedin.com/learning/subscription/topics/ios'],
+              ['iOS & Swift - The Complete iOS App Development Bootcamp','https://www.udemy.com/course/ios-13-app-development-bootcamp/'],
+              ['Become an iOS Developer','https://www.udacity.com/course/ios-developer-nanodegree--nd003'],
+              ['Learn Swift by Codecademy','https://www.codecademy.com/learn/learn-swift'],
+              ['Swift Tutorial - Full Course for Beginners [Free]','https://youtu.be/comQ1-x2a1Q'],
+              ['Learn Swift Fast - [Free]','https://youtu.be/FcsY1YPBwzQ']]
+uiux_course = [['Google UX Design Professional Certificate','https://www.coursera.org/professional-certificates/google-ux-design'],
+               ['UI / UX Design Specialization','https://www.coursera.org/specializations/ui-ux-design'],
+               ['Become a UX Designer by Udacity','https://www.udacity.com/course/ux-designer-nanodegree--nd578'],
+               ['Adobe XD Tutorial: User Experience Design Course [Free]','https://youtu.be/68w2VwalD5w'],
+               ['Adobe XD for Beginners [Free]','https://youtu.be/WEljsc2jorI'],
+               ['Adobe XD in Simple Way','https://learnux.io/course/adobe-xd']]
+
+resume_videos = ['https://youtu.be/y8YH0Qbu5h4','https://youtu.be/J-4Fv8nq1iA',
+                 'https://youtu.be/CLUsplI4xMU','https://youtu.be/pbczsLkv7Cc']
+
+interview_videos = ['https://youtu.be/Ji46s5BHdr0','https://youtu.be/seVxXHi2YMs',
+                    'https://youtu.be/JZK1MZwUyUU','https://youtu.be/CyXLhHQS3KY']
